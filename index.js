ac00f3151515abd08ddd597830e6396d77608bbf
lostmart <martinpedraza1979@gmail.com>
2024-10-01 13:24:54 +0200

    json

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log("hello");
+const express = require("express")
+const app = express()
+const port = 3000
 
+app.use(express.json())
 
+app.get("/", (req, res) => {
+    res.json({
+        msg: "hello from API"
+    })
+})
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`)
+})
